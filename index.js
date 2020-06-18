a66ee6e8c3bf5b28993138053c5c9a3c726fcbff
FreekJanssen <freekwjanssen@gmail.com>
2020-06-18 17:21:39 +0200

    add GET /menu endpoint

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const cors = require("cors");
 
+const menuRouter = require('./routers/menu');
+
 const app = express();
 const port = process.env.PORT || 4000;
 
 app.use(express.json());
 app.use(cors());
+
+app.use('/menu', menuRouter);
 
 app.listen(port, () => {
 	console.log(`Listening on: ${port}`);
